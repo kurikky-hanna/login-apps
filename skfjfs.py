451df2732de384bc676ca034b464e763fa0016e9
kurikky-hanna <kimurarikukun0603@gmail.com>
2026-09-12 12:52:03 +0900

    Refactor streamlit app for improved structure

--- skfjfs.py
+++ skfjfs.py
@@ -1,6 +1,6 @@
 
 import streamlit as st
-import pygame as pyg
+
 import random
 pyg.mixer.init()
 st.title("セキュリティアプリ")
@@ -20,19 +20,17 @@ if codes=="06032511E":
     if not "bal" in st.session_state:
         st.session_state.bal=""
     if name:
-        if st.session_state.names=="verity":
-            pyg.mixer.music.load("[Verity]俺のこと下......呼ばないで.mp3")
-            pyg.mixer.music.play()
-        else:
+
+        
             
-            if st.session_state.bal=="":
-                st.balloons()
-            st.text("サインイン成功！")
-            st.session_state.bal="inst"
-            st.write(f"ようこそ！{st.session_state.names}さん！")
-            st.subheader("軽いゲームをしましょう！")
-            if not "mikuji" in st.session_state:
-                st.session_state.mikuji=["大吉","中吉","小吉","吉","凶"]
+        if st.session_state.bal=="":
+            st.balloons()
+        st.text("サインイン成功！")
+        st.session_state.bal="inst"
+        st.write(f"ようこそ！{st.session_state.names}さん！")
+        st.subheader("軽いゲームをしましょう！")
+        if not "mikuji" in st.session_state:
+            st.session_state.mikuji=["大吉","中吉","小吉","吉","凶"]
             if st.button("おみくじ"):
                 st.write(st.session_state.mikuji[random.randint(0,4)])
             
